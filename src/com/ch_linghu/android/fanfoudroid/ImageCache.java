c8cab7f0a28de6acb7b70c73934872d1e31d0bbb
ch.Linghu <ch.linghu@gmail.com>
2011-01-09 11:43:56 +0800

    删除已经重构过的旧文件。

--- src/com/ch_linghu/android/fanfoudroid/ImageCache.java
+++
@@ -1,8 +0,0 @@
-package com.ch_linghu.android.fanfoudroid;
-
-import android.graphics.Bitmap;
-
-public interface ImageCache {
-  public Bitmap get(String url);
-  public void put(String url, Bitmap bitmap);
-}
--- src/com/ch_linghu/android/fanfoudroid/TweetAdapter.java
+++
@@ -1,7 +0,0 @@
-package com.ch_linghu.android.fanfoudroid;
-
-import android.widget.ListAdapter;
-
-public interface TweetAdapter extends ListAdapter{
-	void refresh();
-}
