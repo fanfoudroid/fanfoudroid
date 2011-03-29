51c499483e56abbe853076e34e4b9af18ff536ff
ch.Linghu <ch.linghu@gmail.com>
2011-03-29 18:58:53 +0800

    release 0.7.1
    修复在1.6下启动FC的问题（由StrictMode类导致）
    修复搜索重复问题(Issue 66)

--- src/com/ch_linghu/fanfoudroid/TwitterApplication.java
+++ src/com/ch_linghu/fanfoudroid/TwitterApplication.java
@@ -46,17 +46,19 @@ public class TwitterApplication extends Application {
 
     @Override
     public void onCreate() {
-        //NOTE: StrictMode模式需要2.3 API支持。
-        if (DEBUG){
-        	StrictMode.setThreadPolicy(new StrictMode.ThreadPolicy.Builder()
-        		.detectAll()
-        		.penaltyLog()
-        		.build());
-        	StrictMode.setVmPolicy(new StrictMode.VmPolicy.Builder()
-        		.detectAll()
-        		.penaltyLog()
-        		.build());
-        }
+    	//FIXME: StrictMode类在1.6以下的版本中没有，会导致类加载失败。
+    	//       因此将这些代码设成关闭状态，仅在做性能调试时才打开。
+//        //NOTE: StrictMode模式需要2.3 API支持。
+//        if (DEBUG){
+//        	StrictMode.setThreadPolicy(new StrictMode.ThreadPolicy.Builder()
+//        		.detectAll()
+//        		.penaltyLog()
+//        		.build());
+//        	StrictMode.setVmPolicy(new StrictMode.VmPolicy.Builder()
+//        		.detectAll()
+//        		.penaltyLog()
+//        		.build());
+//        }
 
 
     	super.onCreate();
