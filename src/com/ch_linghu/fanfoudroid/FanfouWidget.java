a70db63c059618a0924ac012bb10a1f7f2f76362
dinophp@gmail.com <dinophp@gmail.com>
2011-04-11 23:57:47 +0800

    刚形成一个很简陋的widget，只更新了消息，暂时没处理文本格式，每十秒更新一次 Next TODO:头像更新，文本格式处理，个人头像，判断是否登录

--- src/com/ch_linghu/fanfoudroid/FanfouWidget.java
+++ src/com/ch_linghu/fanfoudroid/FanfouWidget.java
@@ -1,26 +1,66 @@
 package com.ch_linghu.fanfoudroid;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
+import java.util.Timer;
+import java.util.TimerTask;
 
+import com.ch_linghu.fanfoudroid.data.Tweet;
+import com.ch_linghu.fanfoudroid.data.db.StatusTable;
+import com.ch_linghu.fanfoudroid.data.db.TwitterDatabase;
 import com.ch_linghu.fanfoudroid.service.TwitterService;
 
 import android.appwidget.AppWidgetProvider;
 import android.content.ComponentName;
 import android.content.Intent;
 import android.content.Context;
+import android.database.Cursor;
+import android.util.Log;
 import android.widget.RemoteViews;
 import android.appwidget.AppWidgetManager;
 
 public class FanfouWidget extends AppWidgetProvider {
-	private String messageText;
+	public final String TAG="com.ch_linghu.fanfoudroid.FanfouWidget";
+	private List<Tweet> tweets;
+	private int position = 0;
 
 	@Override
 	public void onUpdate(Context context, AppWidgetManager appwidgetmanager,
 			int[] appWidgetIds) {
-		ComponentName fanfouWidget = new ComponentName(context,
-				FanfouWidget.class);
-		AppWidgetManager manager = AppWidgetManager.getInstance(context);
-		manager.updateAppWidget(fanfouWidget, buildUpdate(context));
+		Log.i(TAG, "onUpdate");
+	
+
+		 
+		/*position = 0;
+		fetchMessages();
+		Timer timer = new Timer();
+		timer.scheduleAtFixedRate(new MyTime(context, appwidgetmanager), 1,
+				6000);
+		super.onUpdate(context, appwidgetmanager, appWidgetIds);
+		*/
+
+
+	}
+
+	private class MyTime extends TimerTask {
+		RemoteViews remoteViews;
+		AppWidgetManager appWidgetManager;
+		ComponentName thisWidget;
+		Context c;
+
+		public MyTime(Context context, AppWidgetManager appWidgetManager) {
+			this.appWidgetManager = appWidgetManager;
+	
+			this.c = context;
+			thisWidget = new ComponentName(context, FanfouWidget.class);
+		}
+
+		@Override
+		public void run() {
+			position++;
+			this.appWidgetManager.updateAppWidget(thisWidget, buildUpdate(this.c));
+		}
 
 	}
 
@@ -28,28 +68,57 @@ public class FanfouWidget extends AppWidgetProvider {
 
 		RemoteViews updateViews = new RemoteViews(context.getPackageName(),
 				R.layout.widget_initial_layout);
-		updateViews.setTextViewText(R.id.message, messageText);
+		updateViews
+				.setTextViewText(R.id.status_text, tweets.get(position).text);
 		return updateViews;
 
 	}
 
+	public TwitterDatabase getDb() {
+		return TwitterApplication.mDb;
+	}
+
+	public String getUserId() {
+		return TwitterApplication.getMyselfId();
+	}
+
+	private void fetchMessages() {
+		if (tweets == null) {
+			tweets = new ArrayList<Tweet>();
+
+		}
+		Cursor cursor = getDb().fetchAllTweets(getUserId(),
+				StatusTable.TYPE_HOME);
+		if (cursor != null) {
+			if (cursor.moveToFirst()) {
+				do {
+					Tweet tweet = StatusTable.parseCursor(cursor);
+					tweets.add(tweet);
+				} while (cursor.moveToNext());
+			}
+		}
+	}
+
 	@Override
 	public void onDeleted(Context context, int[] appWidgetIds) {
-		// TODO Put your code here
+		Log.i(TAG, "onDeleted");
 	}
 
 	@Override
 	public void onEnabled(Context context) {
-		// TODO Put your code here
+		Log.i(TAG, "onEnabled");
+		tweets = new ArrayList<Tweet>();
 	}
 
 	@Override
 	public void onDisabled(Context context) {
-		// TODO Put your code here
+		Log.i(TAG, "onDisabled");
 	}
 
 	@Override
 	public void onReceive(Context context, Intent intent) {
-		// TODO Put your code here
+		Log.i(TAG, "onReceive");
+		Intent i=new Intent(context,TwitterService.class);
+		context.startService(i);
 	}
 }
--- src/com/ch_linghu/fanfoudroid/service/TwitterService.java
+++ src/com/ch_linghu/fanfoudroid/service/TwitterService.java
@@ -29,17 +29,23 @@ import android.app.Notification;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.app.Service;
+import android.appwidget.AppWidgetManager;
+import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.database.Cursor;
 import android.net.Uri;
+import android.os.Handler;
 import android.os.IBinder;
 import android.os.PowerManager;
 import android.os.PowerManager.WakeLock;
 import android.preference.PreferenceManager;
 import android.util.Log;
+import android.widget.RemoteViews;
 
 import com.ch_linghu.fanfoudroid.DmActivity;
+import com.ch_linghu.fanfoudroid.FanfouWidget;
 import com.ch_linghu.fanfoudroid.MentionActivity;
 import com.ch_linghu.fanfoudroid.R;
 import com.ch_linghu.fanfoudroid.TwitterActivity;
@@ -68,9 +74,73 @@ public class TwitterService extends Service {
 
 	private ArrayList<Tweet> mNewTweets;
 	private ArrayList<Tweet> mNewMentions;
+	
+
 	private ArrayList<Dm> mNewDms;
 
 	private GenericTask mRetrieveTask;
+	
+	private int position=0;
+	
+	public RemoteViews buildUpdate(Context context) {
+
+		RemoteViews updateViews = new RemoteViews(context.getPackageName(),
+				R.layout.widget_initial_layout);
+		updateViews
+				.setTextViewText(R.id.status_text, tweets.get(position).text);
+		return updateViews;
+
+	}
+	private Handler handler=new Handler();
+	
+	private Runnable mTask=new Runnable(){
+
+		@Override
+		public void run() {
+			Log.i(TAG, "position="+position);
+			if(position>tweets.size()){
+				position=0;
+			}
+			position++;
+			 ComponentName fanfouWidget = new ComponentName(TwitterService.this,FanfouWidget.class); 
+			 AppWidgetManager manager = AppWidgetManager.getInstance(getBaseContext());
+			 manager.updateAppWidget(fanfouWidget, buildUpdate(TwitterService.this));
+			 handler.postDelayed(mTask,10000);
+		}
+		
+	};
+	
+	public String getUserId() {
+		return TwitterApplication.getMyselfId();
+	}
+
+	private List<Tweet> tweets;
+	
+	private void fetchMessages() {
+		if (tweets == null) {
+			tweets = new ArrayList<Tweet>();
+
+		}
+		Cursor cursor = getDb().fetchAllTweets(getUserId(),
+				StatusTable.TYPE_HOME);
+		if (cursor != null) {
+			if (cursor.moveToFirst()) {
+				do {
+					Tweet tweet = StatusTable.parseCursor(cursor);
+					tweets.add(tweet);
+				} while (cursor.moveToNext());
+			}
+		}
+	}
+	
+	@Override
+	public void onStart(Intent intent, int startId) {
+		fetchMessages();
+		super.onStart(intent, startId);
+		handler.postDelayed(mTask,10000);
+	}
+	
+	
 	private TaskListener mRetrieveTaskListener = new TaskAdapter(){
 		@Override
 		public void onPostExecute(GenericTask task, TaskResult result) {
@@ -78,7 +148,9 @@ public class TwitterService extends Service {
 				processNewTweets();
 				processNewMentions();
 				processNewDms();
+				
 			}
+			fetchMessages();
 
 			stopSelf();
 		}
