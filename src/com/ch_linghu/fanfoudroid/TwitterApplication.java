ae96092c32aca3805f69f12d5e457944ce0f439e
ch.Linghu <ch.linghu@gmail.com>
2011-04-21 12:43:17 +0800

    [cont.] apply acra on Application

--- src/com/ch_linghu/fanfoudroid/TwitterApplication.java
+++ src/com/ch_linghu/fanfoudroid/TwitterApplication.java
@@ -18,7 +18,21 @@ import com.ch_linghu.fanfoudroid.helper.ProfileImageCacheManager;
 import com.ch_linghu.fanfoudroid.http.HttpException;
 import com.ch_linghu.fanfoudroid.weibo.Configuration;
 import com.ch_linghu.fanfoudroid.weibo.Weibo;
+import org.acra.*;
+import org.acra.annotation.*;
 
+@ReportsCrashes(formKey="dHowMk5LMXQweVJkWGthb1E1T1NUUHc6MQ",
+    mode = ReportingInteractionMode.NOTIFICATION,
+    resNotifTickerText = R.string.crash_notif_ticker_text,
+    resNotifTitle = R.string.crash_notif_title,
+    resNotifText = R.string.crash_notif_text,
+    resNotifIcon = android.R.drawable.stat_notify_error, // optional. default is a warning sign
+    resDialogText = R.string.crash_dialog_text,
+    resDialogIcon = android.R.drawable.ic_dialog_info, //optional. default is a warning sign
+    resDialogTitle = R.string.crash_dialog_title, // optional. default is your application name
+    resDialogCommentPrompt = R.string.crash_dialog_comment_prompt, // optional. when defined, adds a user text field input with this text resource as a label
+    resDialogOkToast = R.string.crash_dialog_ok_toast // optional. displays a Toast message when the user accepts to send a report.
+)
 public class TwitterApplication extends Application {
 
 	public static final String TAG = "TwitterApplication";
