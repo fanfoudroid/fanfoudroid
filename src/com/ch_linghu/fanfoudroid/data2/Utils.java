506fc801da0586e658b5472aef9e8e5ad7e69af2
San Lau <lds2012@gmail.com>
2011-05-16 13:54:18 +0800

    import GSON

---
+++ src/com/ch_linghu/fanfoudroid/data2/Utils.java
@@ -0,0 +1,19 @@
+package com.ch_linghu.fanfoudroid.data2;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+import java.util.TimeZone;
+
+public class Utils {
+    static SimpleDateFormat sdf = new SimpleDateFormat("EEE MMM dd HH:mm:ss z yyyy", Locale.US);
+    
+    public static Date parseDate(String str, String format) throws ParseException {
+        if (str == null || "".equals(str)) {
+            return null;
+        }
+        sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
+        return sdf.parse(str);
+    }
+}
