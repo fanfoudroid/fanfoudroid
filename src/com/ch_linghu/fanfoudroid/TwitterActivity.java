9e8dc240c9d15e9b6ebc82a42f98eaa87dbbfc60
San Lau <lds2012@gmail.com>
2011-05-19 23:35:22 +0800

    实验滑动功能, 该特性主要逻辑已加入到主分支中的TwitterActicity中, 但暂未实现具体功能, 目的仅在于测试其是否会对已有功能造成影响.

--- src/com/ch_linghu/fanfoudroid/TwitterActivity.java
+++ src/com/ch_linghu/fanfoudroid/TwitterActivity.java
@@ -26,17 +26,19 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.ContextMenu;
 import android.view.ContextMenu.ContextMenuInfo;
+import android.view.GestureDetector;
+import android.view.GestureDetector.SimpleOnGestureListener;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.MotionEvent;
 import android.view.View;
+import android.view.View.OnTouchListener;
 import android.widget.AdapterView;
-import android.widget.Toast;
 import android.widget.AdapterView.AdapterContextMenuInfo;
 
 import com.ch_linghu.fanfoudroid.data.Tweet;
 import com.ch_linghu.fanfoudroid.db.StatusTable;
 import com.ch_linghu.fanfoudroid.http.HttpException;
-import com.ch_linghu.fanfoudroid.http.HttpRefusedException;
 import com.ch_linghu.fanfoudroid.task.GenericTask;
 import com.ch_linghu.fanfoudroid.task.TaskAdapter;
 import com.ch_linghu.fanfoudroid.task.TaskListener;
@@ -48,208 +50,313 @@ import com.ch_linghu.fanfoudroid.weibo.Paging;
 import com.ch_linghu.fanfoudroid.weibo.Status;
 
 public class TwitterActivity extends TwitterCursorBaseActivity {
-	private static final String TAG = "TwitterActivity";
+    private static final String TAG = "TwitterActivity";
 
-	private static final String LAUNCH_ACTION = "com.ch_linghu.fanfoudroid.TWEETS";
-	protected GenericTask mDeleteTask;
+    private static final String LAUNCH_ACTION = "com.ch_linghu.fanfoudroid.TWEETS";
+    protected GenericTask mDeleteTask;
 
-	private TaskListener mDeleteTaskListener = new TaskAdapter() {
+    private TaskListener mDeleteTaskListener = new TaskAdapter() {
 
-		@Override
-		public String getName() {
-			return "DeleteTask";
-		}
+        @Override
+        public String getName() {
+            return "DeleteTask";
+        }
 
-		@Override
-		public void onPostExecute(GenericTask task, TaskResult result) {
-			if (result == TaskResult.AUTH_ERROR) {
-				logout();
-			} else if (result == TaskResult.OK) {
-				onDeleteSuccess();
-			} else if (result == TaskResult.IO_ERROR) {
-				onDeleteFailure();
-			}
-		}
-	};
+        @Override
+        public void onPostExecute(GenericTask task, TaskResult result) {
+            if (result == TaskResult.AUTH_ERROR) {
+                logout();
+            } else if (result == TaskResult.OK) {
+                onDeleteSuccess();
+            } else if (result == TaskResult.IO_ERROR) {
+                onDeleteFailure();
+            }
+        }
+    };
 
-	static final int DIALOG_WRITE_ID = 0;
+    static final int DIALOG_WRITE_ID = 0;
 
-	public static Intent createIntent(Context context) {
-		Intent intent = new Intent(LAUNCH_ACTION);
-		intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+    public static Intent createIntent(Context context) {
+        Intent intent = new Intent(LAUNCH_ACTION);
+        intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
 
-		return intent;
-	}
+        return intent;
+    }
 
-	public static Intent createNewTaskIntent(Context context) {
-		Intent intent = createIntent(context);
-		intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+    public static Intent createNewTaskIntent(Context context) {
+        Intent intent = createIntent(context);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
 
-		return intent;
-	}
+        return intent;
+    }
 
-	@Override
-	protected boolean _onCreate(Bundle savedInstanceState) {
-		if (super._onCreate(savedInstanceState)) {
-			setHeaderTitle("饭否fanfou.com");
-			return true;
-		} else {
-			return false;
-		}
-	}
+    @Override
+    protected boolean _onCreate(Bundle savedInstanceState) {
+        if (super._onCreate(savedInstanceState)) {
+            setHeaderTitle("饭否fanfou.com");
+            registerGestureListener();
+            return true;
+        } else {
+            return false;
+        }
+    }
 
-	@Override
-	protected void onDestroy() {
-		// TODO Auto-generated method stub
-		super.onDestroy();
+    @Override
+    protected void onDestroy() {
+        // TODO Auto-generated method stub
+        super.onDestroy();
 
-		if (mDeleteTask != null
-				&& mDeleteTask.getStatus() == GenericTask.Status.RUNNING) {
-			mDeleteTask.cancel(true);
-		}
-	}
+        if (mDeleteTask != null
+                && mDeleteTask.getStatus() == GenericTask.Status.RUNNING) {
+            mDeleteTask.cancel(true);
+        }
+    }
 
-	@Override
-	protected void onSaveInstanceState(Bundle outState) {
-		super.onSaveInstanceState(outState);
-		if (mDeleteTask != null
-				&& mDeleteTask.getStatus() == GenericTask.Status.RUNNING) {
-			outState.putBoolean(SIS_RUNNING_KEY, true);
-		}
-	}
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        if (mDeleteTask != null
+                && mDeleteTask.getStatus() == GenericTask.Status.RUNNING) {
+            outState.putBoolean(SIS_RUNNING_KEY, true);
+        }
+    }
 
-	// Menu.
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		return super.onCreateOptionsMenu(menu);
-	}
+    // Menu.
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        return super.onCreateOptionsMenu(menu);
+    }
 
-	private int CONTEXT_DELETE_ID = getLastContextMenuId() + 1;
+    private int CONTEXT_DELETE_ID = getLastContextMenuId() + 1;
 
-	@Override
-	protected int getLastContextMenuId() {
-		return CONTEXT_DELETE_ID;
-	}
+    @Override
+    protected int getLastContextMenuId() {
+        return CONTEXT_DELETE_ID;
+    }
 
-	@Override
-	public void onCreateContextMenu(ContextMenu menu, View v,
-			ContextMenuInfo menuInfo) {
-		super.onCreateContextMenu(menu, v, menuInfo);
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v,
+            ContextMenuInfo menuInfo) {
+        super.onCreateContextMenu(menu, v, menuInfo);
 
-		AdapterView.AdapterContextMenuInfo info = (AdapterContextMenuInfo) menuInfo;
-		Tweet tweet = getContextItemTweet(info.position);
-		if (null != tweet) {// 当按钮为 刷新/更多的时候为空
+        AdapterView.AdapterContextMenuInfo info = (AdapterContextMenuInfo) menuInfo;
+        Tweet tweet = getContextItemTweet(info.position);
+        if (null != tweet) {// 当按钮为 刷新/更多的时候为空
 
-			if (tweet.userId.equals(TwitterApplication.getMyselfId())) {
-				menu.add(0, CONTEXT_DELETE_ID, 0, R.string.cmenu_delete);
-			}
+            if (tweet.userId.equals(TwitterApplication.getMyselfId())) {
+                menu.add(0, CONTEXT_DELETE_ID, 0, R.string.cmenu_delete);
+            }
 
-		}
-	}
+        }
+    }
 
-	@Override
-	public boolean onContextItemSelected(MenuItem item) {
-		AdapterContextMenuInfo info = (AdapterContextMenuInfo) item
-				.getMenuInfo();
-		Tweet tweet = getContextItemTweet(info.position);
+    @Override
+    public boolean onContextItemSelected(MenuItem item) {
+        AdapterContextMenuInfo info = (AdapterContextMenuInfo) item
+                .getMenuInfo();
+        Tweet tweet = getContextItemTweet(info.position);
 
-		if (tweet == null) {
-			Log.w(TAG, "Selected item not available.");
-			return super.onContextItemSelected(item);
-		}
+        if (tweet == null) {
+            Log.w(TAG, "Selected item not available.");
+            return super.onContextItemSelected(item);
+        }
 
-		if (item.getItemId() == CONTEXT_DELETE_ID) {
-			doDelete(tweet.id);
-			return true;
-		} else {
-			return super.onContextItemSelected(item);
-		}
-	}
+        if (item.getItemId() == CONTEXT_DELETE_ID) {
+            doDelete(tweet.id);
+            return true;
+        } else {
+            return super.onContextItemSelected(item);
+        }
+    }
 
-	@Override
-	protected Cursor fetchMessages() {
-		return getDb().fetchAllTweets(getUserId(), StatusTable.TYPE_HOME);
-	}
+    @SuppressWarnings("deprecation")
+    @Override
+    protected Cursor fetchMessages() {
+        return getDb().fetchAllTweets(getUserId(), StatusTable.TYPE_HOME);
+    }
 
-	@Override
-	protected String getActivityTitle() {
-		return getResources().getString(R.string.page_title_home);
-	}
+    @Override
+    protected String getActivityTitle() {
+        return getResources().getString(R.string.page_title_home);
+    }
 
-	@Override
-	protected void markAllRead() {
-		getDb().markAllTweetsRead(getUserId(), StatusTable.TYPE_HOME);
-	}
+    @Override
+    protected void markAllRead() {
+        getDb().markAllTweetsRead(getUserId(), StatusTable.TYPE_HOME);
+    }
 
-	// hasRetrieveListTask interface
-	@Override
-	public int addMessages(ArrayList<Tweet> tweets, boolean isUnread) {
-		// 获取消息的时候，将status里获取的user也存储到数据库
-		
-		// ::MARK::
-		for (Tweet t : tweets) {
-			getDb().createWeiboUserInfo(t.user);
-		}
-		return getDb().putTweets(tweets, getUserId(), StatusTable.TYPE_HOME,
-				isUnread);
-	}
+    // hasRetrieveListTask interface
+    @Override
+    public int addMessages(ArrayList<Tweet> tweets, boolean isUnread) {
+        // 获取消息的时候，将status里获取的user也存储到数据库
 
-	@Override
-	public String fetchMaxId() {
-		return getDb().fetchMaxTweetId(getUserId(), StatusTable.TYPE_HOME);
-	}
+        // ::MARK::
+        for (Tweet t : tweets) {
+            getDb().createWeiboUserInfo(t.user);
+        }
+        return getDb().putTweets(tweets, getUserId(), StatusTable.TYPE_HOME,
+                isUnread);
+    }
 
-	@Override
-	public List<Status> getMessageSinceId(String maxId) throws HttpException {
-		if (maxId != null) {
-			return getApi().getFriendsTimeline(new Paging(maxId));
-		} else {
-			return getApi().getFriendsTimeline();
-		}
-	}
+    @Override
+    public String fetchMaxId() {
+        return getDb().fetchMaxTweetId(getUserId(), StatusTable.TYPE_HOME);
+    }
 
-	public void onDeleteFailure() {
-		Log.e(TAG, "Delete failed");
-	}
+    @Override
+    public List<Status> getMessageSinceId(String maxId) throws HttpException {
+        if (maxId != null) {
+            return getApi().getFriendsTimeline(new Paging(maxId));
+        } else {
+            return getApi().getFriendsTimeline();
+        }
+    }
 
-	public void onDeleteSuccess() {
-		mTweetAdapter.refresh();
-	}
+    public void onDeleteFailure() {
+        Log.e(TAG, "Delete failed");
+    }
 
-	private void doDelete(String id) {
+    public void onDeleteSuccess() {
+        mTweetAdapter.refresh();
+    }
 
-		if (mDeleteTask != null
-				&& mDeleteTask.getStatus() == GenericTask.Status.RUNNING) {
-			return;
-		} else {
-			mDeleteTask = new TweetCommonTask.DeleteTask(this);
-			mDeleteTask.setListener(mDeleteTaskListener);
+    private void doDelete(String id) {
 
-			TaskParams params = new TaskParams();
-			params.put("id", id);
-			mDeleteTask.execute(params);
-		}
-	}
+        if (mDeleteTask != null
+                && mDeleteTask.getStatus() == GenericTask.Status.RUNNING) {
+            return;
+        } else {
+            mDeleteTask = new TweetCommonTask.DeleteTask(this);
+            mDeleteTask.setListener(mDeleteTaskListener);
 
-	@Override
-	public String fetchMinId() {
-		return getDb().fetchMinTweetId(getUserId(), StatusTable.TYPE_HOME);
-	}
+            TaskParams params = new TaskParams();
+            params.put("id", id);
+            mDeleteTask.execute(params);
+        }
+    }
 
-	@Override
-	public List<Status> getMoreMessageFromId(String minId) throws HttpException {
-		Paging paging = new Paging(1, 20);
-		paging.setMaxId(minId);
-		return getApi().getFriendsTimeline(paging);
-	}
+    @Override
+    public String fetchMinId() {
+        return getDb().fetchMinTweetId(getUserId(), StatusTable.TYPE_HOME);
+    }
 
-	@Override
-	public int getDatabaseType() {
-		return StatusTable.TYPE_HOME;
-	}
+    @Override
+    public List<Status> getMoreMessageFromId(String minId) throws HttpException {
+        Paging paging = new Paging(1, 20);
+        paging.setMaxId(minId);
+        return getApi().getFriendsTimeline(paging);
+    }
 
-	@Override
-	public String getUserId() {
-		return TwitterApplication.getMyselfId();
-	}
+    @Override
+    public int getDatabaseType() {
+        return StatusTable.TYPE_HOME;
+    }
+
+    @Override
+    public String getUserId() {
+        return TwitterApplication.getMyselfId();
+    }
+
+    // ////////////////// Gesture test /////////////////////////////////////
+    protected MyGestureListener myGestureListener;
+
+    private void registerGestureListener() {
+
+        myGestureListener = new MyGestureListener(this);
+        // or if you have already created a Gesture Detector.
+        // myGestureListener = new MyGestureListener(this,
+        // getExistingGestureDetector());
+
+        // Example of setting listener. The onTouchEvent will now be called on
+        // your listener
+        getTweetList().setOnTouchListener(myGestureListener);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        // or implement in activity or component. When your not assigning to a
+        // child component.
+        return myGestureListener.getDetector().onTouchEvent(event);
+    }
+
+    class MyGestureListener extends SimpleOnGestureListener implements
+            OnTouchListener {
+        private static final int SWIPE_MIN_DISTANCE = 120;
+        private static final int SWIPE_MAX_OFF_PATH = 250;
+        private static final int SWIPE_THRESHOLD_VELOCITY = 200;
+
+        Context context;
+        GestureDetector gDetector;
+
+        public MyGestureListener() {
+            super();
+        }
+
+        public MyGestureListener(Context context) {
+            this(context, null);
+        }
+
+        public MyGestureListener(Context context, GestureDetector gDetector) {
+
+            if (gDetector == null)
+                gDetector = new GestureDetector(context, this);
+
+            this.context = context;
+            this.gDetector = gDetector;
+        }
+
+        @Override
+        public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX,
+                float velocityY) {
+            Log.d("FLING", "On fling");
+            boolean orig = super.onFling(e1, e2, velocityX, velocityY);
+
+            try {
+                if (Math.abs(e1.getY() - e2.getY()) > SWIPE_MAX_OFF_PATH) {
+                    return orig;
+                }
+                if (e1.getX() - e2.getX() > SWIPE_MIN_DISTANCE
+                        && Math.abs(velocityX) > SWIPE_THRESHOLD_VELOCITY) {
+                    // right to left swipe
+                    Log.d("FLING", "<------");
+                    return true;
+                } else if (e2.getX() - e1.getX() > SWIPE_MIN_DISTANCE
+                        && Math.abs(velocityX) > SWIPE_THRESHOLD_VELOCITY) {
+                    Log.d("FLING", "------>");
+                    return true;
+                }
+            } catch (Exception e) {
+                Log.e(TAG, e.getMessage());
+            }
+
+            return orig;
+        }
+
+        @Override
+        public void onLongPress(MotionEvent e) {
+            // TODO Auto-generated method stub
+            super.onLongPress(e);
+        }
+
+        /*
+         * @Override public boolean onSingleTapConfirmed(MotionEvent e) {
+         * 
+         * return super.onSingleTapConfirmed(e); }
+         */
+        public boolean onTouch(View v, MotionEvent event) {
+            // Log.d("FLING", "On Touch");
+
+            // Within the MyGestureListener class you can now manage the
+            // event.getAction() codes.
+
+            // Note that we are now calling the gesture Detectors onTouchEvent.
+            // And given we've set this class as the GestureDetectors listener
+            // the onFling, onSingleTap etc methods will be executed.
+            return gDetector.onTouchEvent(event);
+        }
+
+        public GestureDetector getDetector() {
+            return gDetector;
+        }
+    }
 }
