0877c37f7f4a411d5c1d32e7c28a5fac4a96a187
San Lau <lds2012@gmail.com>
2011-05-24 14:59:42 +0800

    LazyImageLoader#ImageManager使用线程安全的ConcurrentHashMap来替代HashMap

--- src/com/ch_linghu/fanfoudroid/app/LazyImageLoader.java
+++ src/com/ch_linghu/fanfoudroid/app/LazyImageLoader.java
@@ -3,9 +3,9 @@ package com.ch_linghu.fanfoudroid.app;
 import java.io.IOException;
 import java.lang.Thread.State;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.TimeUnit;
 
 import android.graphics.Bitmap;
@@ -16,52 +16,14 @@ import android.util.Log;
 
 import com.ch_linghu.fanfoudroid.TwitterApplication;
 
-class CallbackManager{
-	private static final String TAG = "CallbackManager";
-	private HashMap<String, ArrayList<LazyImageLoader.ImageLoaderCallback>> mCallbackMap;
-	
-	public CallbackManager(){
-		mCallbackMap = new HashMap<String, ArrayList<LazyImageLoader.ImageLoaderCallback>>();
-	}
-	
-	public void put(String url, LazyImageLoader.ImageLoaderCallback callback){
-		Log.d(TAG, "url="+url);
-		if (!mCallbackMap.containsKey(url)){
-			Log.d(TAG, "url does not exist, add list to map");
-			mCallbackMap.put(url, new ArrayList<LazyImageLoader.ImageLoaderCallback>());
-		}
-		
-		mCallbackMap.get(url).add(callback);
-		Log.d(TAG, "Add callback to list, count(url)=" + mCallbackMap.get(url).size());
-	}
-	
-	public void call(String url, Bitmap bitmap){
-		Log.d(TAG, "call url=" + url);
-		ArrayList<LazyImageLoader.ImageLoaderCallback> callbackList = mCallbackMap.get(url);
-		if (callbackList == null){
-			//FIXME: 有时会到达这里，原因我还没想明白
-			Log.d(TAG, "callbackList=null");				
-			return;
-		}
-		for (LazyImageLoader.ImageLoaderCallback callback : callbackList){
-			if(callback != null){
-				callback.refresh(url, bitmap);
-			}
-		}
-
-		callbackList.clear();
-		mCallbackMap.remove(url);
-	}
-	
-	
-}
 public class LazyImageLoader {
     private static final String TAG = "ProfileImageCacheManager";
     public static final int HANDLER_MESSAGE_ID = 1;
     public static final String EXTRA_BITMAP = "extra_bitmap";
     public static final String EXTRA_IMAGE_URL = "extra_image_url";
 
-    private ImageManager mImageManager = new ImageManager(TwitterApplication.mContext);
+    private ImageManager mImageManager = new ImageManager(
+            TwitterApplication.mContext);
     private BlockingQueue<String> mUrlList = new ArrayBlockingQueue<String>(50);
     private CallbackManager mCallbackManager = new CallbackManager();
 
@@ -69,7 +31,7 @@ public class LazyImageLoader {
 
     public Bitmap get(String url, ImageLoaderCallback callback) {
         Bitmap bitmap = ImageCache.mDefaultBitmap;
-        if(mImageManager.isContains(url)){
+        if (mImageManager.isContains(url)) {
             bitmap = mImageManager.get(url);
         } else {
             // bitmap不存在，启动Task进行下载
@@ -101,10 +63,10 @@ public class LazyImageLoader {
         }
 
         // start thread if it's not started yet
-        Log.i("LDS", mTask.getState() +"");
-        
+        Log.i("LDS", mTask.getState() + "");
+
         State state = mTask.getState();
-        if (Thread.State.NEW == state) { 
+        if (Thread.State.NEW == state) {
             mTask.start();
         } else if (Thread.State.TERMINATED == state) {
             mTask = new GetImageTask(); // restart thread
@@ -114,26 +76,28 @@ public class LazyImageLoader {
 
     private class GetImageTask extends Thread {
         private volatile boolean mTaskTerminated = false;
-        private static final int TIMEOUT = 3*60; 
+        private static final int TIMEOUT = 3 * 60;
         private boolean isPermanent = true;
-        
+
         @Override
-		public void run() {
+        public void run() {
             try {
-                while ( !mTaskTerminated ) {
+                while (!mTaskTerminated) {
                     String url;
                     if (isPermanent) {
                         url = mUrlList.take();
                     } else {
                         url = mUrlList.poll(TIMEOUT, TimeUnit.SECONDS); // waiting
-                        if (null == url) { break; } // no more, shutdown
+                        if (null == url) {
+                            break;
+                        } // no more, shutdown
                     }
-                    
-                    Bitmap bitmap = ImageCache.mDefaultBitmap;
-                    bitmap = mImageManager.safeGet(url);
+
+                    // Bitmap bitmap = ImageCache.mDefaultBitmap;
+                    final Bitmap bitmap = mImageManager.safeGet(url);
 
                     // use handler to process callback
-                    Message m = handler.obtainMessage(HANDLER_MESSAGE_ID);
+                    final Message m = handler.obtainMessage(HANDLER_MESSAGE_ID);
                     Bundle bundle = m.getData();
                     bundle.putString(EXTRA_IMAGE_URL, url);
                     bundle.putParcelable(EXTRA_BITMAP, bitmap);
@@ -148,15 +112,18 @@ public class LazyImageLoader {
                 mTaskTerminated = true;
             }
         }
-            
+
+        @SuppressWarnings("unused")
         public boolean isPermanent() {
             return isPermanent;
         }
 
+        @SuppressWarnings("unused")
         public void setPermanent(boolean isPermanent) {
             this.isPermanent = isPermanent;
         }
 
+        @SuppressWarnings("unused")
         public void shutDown() throws InterruptedException {
             mTaskTerminated = true;
         }
@@ -164,25 +131,62 @@ public class LazyImageLoader {
 
     Handler handler = new Handler() {
         @Override
-		public void handleMessage(Message msg) {
+        public void handleMessage(Message msg) {
             switch (msg.what) {
-                case HANDLER_MESSAGE_ID:
-                    Bundle bundle = msg.getData();
-                    String url = bundle.getString(EXTRA_IMAGE_URL);
-                    Bitmap bitmap = (Bitmap) (bundle.get(EXTRA_BITMAP));
+            case HANDLER_MESSAGE_ID:
+                final Bundle bundle = msg.getData();
+                String url = bundle.getString(EXTRA_IMAGE_URL);
+                Bitmap bitmap = (Bitmap) (bundle.get(EXTRA_BITMAP));
 
-                    // callback
-                    mCallbackManager.call(url, bitmap);
-                    break;
-                default:
-                    // do nothing.
+                // callback
+                mCallbackManager.call(url, bitmap);
+                break;
+            default:
+                // do nothing.
             }
         }
     };
-    
+
     public interface ImageLoaderCallback {
         void refresh(String url, Bitmap bitmap);
     }
+
+    public static class CallbackManager {
+        private static final String TAG = "CallbackManager";
+        private ConcurrentHashMap<String, ArrayList<ImageLoaderCallback>> mCallbackMap;
+
+        public CallbackManager() {
+            mCallbackMap = new ConcurrentHashMap<String, ArrayList<ImageLoaderCallback>>();
+        }
+
+        public void put(String url, ImageLoaderCallback callback) {
+            Log.v(TAG, "url=" + url);
+            if (!mCallbackMap.containsKey(url)) {
+                Log.v(TAG, "url does not exist, add list to map");
+                mCallbackMap.put(url, new ArrayList<ImageLoaderCallback>());
+            }
+
+            mCallbackMap.get(url).add(callback);
+            Log.v(TAG, "Add callback to list, count(url)=" + mCallbackMap.get(url).size());
+        }
+
+        public void call(String url, Bitmap bitmap) {
+            Log.v(TAG, "call url=" + url);
+            ArrayList<ImageLoaderCallback> callbackList = mCallbackMap.get(url);
+            if (callbackList == null) {
+                // FIXME: 有时会到达这里，原因我还没想明白
+                Log.e(TAG, "callbackList=null");
+                return;
+            }
+            for (ImageLoaderCallback callback : callbackList) {
+                if (callback != null) {
+                    callback.refresh(url, bitmap);
+                }
+            }
+
+            callbackList.clear();
+            mCallbackMap.remove(url);
+        }
+
+    }
 }
-
-
